fb7c493732ee13de839ae6ad8b54268bc4969eb0
Brian Butz <butzopower@gmail.com>
2010-02-15 20:10:59 -0600

    Made it stop playing moves that didn't move pieces, speeding things up greatly

--- proj1/board.rb
+++ proj1/board.rb
@@ -17,12 +17,24 @@ class Board
   end
 
   def boards_for_moves_of_player(player)
-    (0..5).to_a.map do |x|
+    valid_moves = (0..5).to_a.select{|x| player.row.houses[x] > 0} # can't make moves for 0
+
+    if valid_moves.empty? # game is over, can't make valid move
+      # score is current score plus all pieces
+      p1_score = player.score + player.row.houses.inject(0){|x, y| x + y}
+      p2_score = player.opponent.score + player.opponent.row.houses.inject(0){|x, y| x + y} 
+
+      new_player1 = Player.new(Array.new(6){0}, p1_score) # player with new score and board of 6 0s
+      new_player2 = Player.new(Array.new(6){0}, p2_score) # player with new score and board of 6 0s
+      return [[Board.new(new_player1, new_player2), nil]] # hopefully Board will figure out somebody won
+    end
+
+    valid_moves.map do |x|
       new_player1 = Player.new(player.row.houses, player.score)
       new_player2 = Player.new(player.opponent.row.houses, player.opponent.score)
       board = Board.new(new_player1, new_player2) # need to do this first to setup opponents
       new_player1.row.apply(x) 
-      board
+      [board, x+1]
     end 
   end
   
--- proj1/minimax.rb
+++ proj1/minimax.rb
@@ -11,11 +11,13 @@ module Minimax
   
   def self.search(board, player, horizon)
     # lost if opponent won
-    return -infinity if board.won_by?(player.opponent)
+    return [-infinity, nil] if board.won_by?(player.opponent)
 
-    return board.evaluate_for_player(player) if horizon == 0
+    return [board.evaluate_for_player(player), nil] if horizon == 0
 
-    return board.boards_for_moves_of_player(player).map {|new_board| -self.search(new_board, new_board.first_player.opponent, horizon -1)}.max
+    return board.boards_for_moves_of_player(player).compact.map do |new_board, move| 
+             [-1 * self.search(new_board, new_board.first_player.opponent, horizon -1).first, move]
+           end.max
   end
 
 end
