f81e960f9d69be744d0180423aa2448a9713515d
Brian Butz <butzopower@gmail.com>
2010-02-15 18:46:58 -0600

    proj1: have game play modelled, basic minimax that returns scores working

--- proj1/board.rb
+++ proj1/board.rb
@@ -1,10 +1,11 @@
 class Board
   # ruby utility method for creating setters and getters
-  attr_accessor :player_one, :player_two
+  attr_accessor :first_player
 
   def initialize(player_one, player_two)
     player_one.opponent = player_two
     player_two.opponent = player_one
+    self.first_player = player_one
   end
 
   def won_by?(player)
@@ -16,8 +17,12 @@ class Board
   end
 
   def boards_for_moves_of_player(player)
-    0.upto(5).map do |x|
-
+    (0..5).to_a.map do |x|
+      new_player1 = Player.new(player.row.houses, player.score)
+      new_player2 = Player.new(player.opponent.row.houses, player.opponent.score)
+      board = Board.new(new_player1, new_player2) # need to do this first to setup opponents
+      new_player1.row.apply(x) 
+      board
     end 
   end
   
--- proj1/row.rb
+++ proj1/row.rb
@@ -9,20 +9,48 @@ class Row
   # initial call to start moving pieces
   def apply(position)
     remaining = houses[position] # get the number of pieces
-    houses[position] = 0 # empty out the house
+    self.houses[position] = 0 # empty out the house
     (position + 1).upto(5) do |pos| # for all houses remaining on our row
       if remaining > 0              # while we still have pieces
-        houses[pos] += 1              # increase them by one
+        self.houses[pos] += 1              # increase them by one
         remaining -= 1                # decrease remaining by one
       end
     end
 
     if remaining > 0
-      player.opponent.row.apply_moves_from_player(player, position, remaining)
+      self.player.opponent.row.apply_moves_from_player(self.player, position, remaining)
+    end
   end
 
-  def apply_moves_from_player(applying_player, position, remaining)
+  def apply_moves_from_player(applying_player, applied_position, remaining)
 
+    range = (0..5).to_a                                   # create an array 0 to 5
+    range -= [applied_position] if self.player == applying_player # skip the one we emptied 
+                                                          #if we are the applying player
+    
+    range = range.first(remaining) # get as many as we have remaining, or all of them
+
+    range.each do |pos|
+      self.houses[pos] += 1
+      remaining -= 1
+    end
+
+    if remaining == 0 && self.player == applying_player.opponent
+      score_for_player_at_position(applying_player, range.last)
+    end
+
+    if remaining > 0
+      self.player.opponent.row.apply_moves_from_player(applying_player, applied_position, remaining)
+    end
   end
 
+  def score_for_player_at_position(player, position)
+    unless position == 5 && houses.all?{|x| x >= 2 && x <= 3} # don't do anything if we would wipe the whole board
+      while position >= 0 && (houses[position] == 2 || houses[position] == 3)
+        player.score += houses[position]
+        houses[position] = 0
+        position -= 1
+      end
+    end
+  end
 end
